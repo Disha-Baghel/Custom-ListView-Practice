c821b0568ac1d01f387d9f3f237a7318cb2d9662
Disha-Baghel <bagheldisha708@gmail.com>
2023-01-25 20:58:56 +0530

    Done changes

--- app/src/main/java/com/example/worldcupapplication/CustomAdapter.java
+++ app/src/main/java/com/example/worldcupapplication/CustomAdapter.java
@@ -1,6 +1,7 @@
 package com.example.worldcupapplication;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -42,7 +43,41 @@ public class CustomAdapter extends ArrayAdapter<CountryModelClass> {
         //Check if an existing view is being reused, otherwise inflate the view
         ViewHolder viewHolder;
         final View result;
-        if(convertView == )
+        if(convertView == null){
+            viewHolder = new ViewHolder();
 
+            //Layout Inflater is a class used to instantiate Layout XML file into its corresponding
+            //view object which can be used in java programs
+            //There are two ways tp create UI in android i)Static  ii)Dynamic of Programmatically
+
+            //Inflating means reading the XML file that describes the layout or GUI Element.and to
+            //create the actual object that correspond to it and thus making the object visible
+            //within an android app.
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+
+            convertView = inflater.inflate(
+                    R.layout.item_list_layout,
+                    parent,
+                    false
+            );
+            viewHolder.txtCountry = (TextView) convertView.findViewById(R.id.countryName);
+            viewHolder.txtCupWins = (TextView) convertView.findViewById(R.id.cup_win_desc);
+            viewHolder.flagImg = (ImageView) convertView.findViewById(R.id.imageView);
+
+            result = convertView;
+            convertView.setTag(viewHolder);
+        }
+        else{
+            viewHolder = (ViewHolder) convertView.getTag();
+            result = convertView;
+        }
+
+        //Get the data form the model class
+        viewHolder.txtCountry.setText(dataModel.getCountry_name());
+        viewHolder.txtCupWins.setText(dataModel.getCup_win_count());
+        viewHolder.flagImg.setImageResource(dataModel.getFlag_img());
+
+        return convertView;
     }
 }
+
--- app/src/main/java/com/example/worldcupapplication/MainActivity.java
+++ app/src/main/java/com/example/worldcupapplication/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.worldcupapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView ;
+    private static CustomAdapter adapter;
+    ArrayList<CountryModelClass> dataModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //1. AdapterView: a listView
+        listView = findViewById(R.id.listview);
+        //2. Data source
+        dataModels = new ArrayList<>();
+        dataModels.add(new CountryModelClass("Germany", 4, R.drawable.germany));
+        dataModels.add(new CountryModelClass("France", 2, R.drawable.img));
+        dataModels.add(new CountryModelClass("Germany", 4, R.drawable.germany));
+        dataModels.add(new CountryModelClass("Germany", 4, R.drawable.germany));
+        dataModels.add(new CountryModelClass("Germany", 4, R.drawable.germany));
+        dataModels.add(new CountryModelClass("Germany", 4, R.drawable.germany));
+        dataModels.add(new CountryModelClass("Germany", 4, R.drawable.germany));
+        dataModels.add(new CountryModelClass("Germany", 4, R.drawable.germany));
+        dataModels.add(new CountryModelClass("Germany", 4, R.drawable.germany));
+        dataModels.add(new CountryModelClass("Germany", 4, R.drawable.germany));
+        dataModels.add(new CountryModelClass("Germany", 4, R.drawable.germany));
+
+        //3. Adapter
+        adapter = new CustomAdapter(dataModels, getApplicationContext());
+        listView.setAdapter(adapter);
+
     }
 }
